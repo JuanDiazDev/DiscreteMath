ff971271da2f691be71f4ea9443dc2c99c7b2bb1
Juan Diaz <juanmanuel32@ciencias.unam.mx>
2020-11-18 16:22:49 -0700

    Add files via upload

---
+++ Practica2.hs
@@ -0,0 +1,99 @@
+-- | Estructuras Discretas 2020-1
+-- | Práctica 2: Recursión sobre listas
+-- | Profesora: Pilar Selene Linares Arévalo
+-- | Laboratorio: Adrián Felipe Vélez Rivera
+-- | Integrantes: 420004365 juanmanuel32@ciencias.unam.mx
+
+
+module Practica2 where
+
+--fibonacci. Regresa el n-ésimo elemento de una serie de fibonacci
+--fibonacci 3 = 2
+fibonacci :: Int -> Int
+fibonnaci 0 = 0
+fibonacci 1 = 1
+fibonacci x = fibonacci (x-1) + fibonacci (x-2)
+
+--andy. Dada una lista de booleanos, regresa si todos los elementos de la lista se evalúan a verdadero.
+--andy [True, True, False, True] -> False
+andy :: [Bool] -> Bool
+andy [] = True
+andy (x:xs) = if x == True then (andy xs) else False
+
+--concate. Concatena una lista de listas.
+--concate [[1,2],[3,4],[5]] = [1,2,3,4,5]
+concate :: [[a]] -> [a]
+concate [[x]]=[x]
+concate [[]] = []
+concate [xs,ys] = xs ++ concate [ys]
+
+--esElemento. Regresa si un elemento pertenece a una lista o no.
+--esElemento 5 [1,2,3,4] = False
+esElemento :: Eq a => a -> [a] -> Bool
+esElemento a [] = False
+esElemento a (x:xs) = if a == x then True else (esElemento a xs)
+
+--merge. Concatena dos listas y la lista output corresponde a una lista ordenada.
+--merge [3,5,1] [4,2] = [1,2,3,4,5]
+merge :: Ord a => [a] -> [a] -> [a]
+merge (x:xs) [] = isort(x:xs)
+merge [] [] = []
+merge [] (x:xs) = isort(x:xs)
+merge (x:xs) (y:ys) = isort([x] ++ (merge (xs) (y:ys)))
+
+--inversa. Dada una lista de tuplas, regresa otra lista donde el primer y segundo valor de cada tupla se intercambia de posición.
+--inversa [(1,2),(1,2)] = [(2,1),(2,1)]
+inversa [] = []
+inversa list = [(d,c)|(c,d)<-list]
+
+--elimRep. Elimina todos los elementos repetidos de una lista.
+--elimRep [1,1,2,2,3,3,4,4] -> [1,2,3,4]
+elimRep :: (Eq a) => [a] -> [a]
+elimRep (x:xs) = x : elimRep (filter (/= x) xs)
+elimRep [] = []
+
+--eliminaElem. Elimina todas las ocurrencias de un elemento en una lista.
+--eliminaElem 4 [1,2,3,4,4] -> [1,2,3]
+eliminaElem :: Eq a => a -> [a] -> [a]
+eliminaElem _ [] = []
+eliminaElem x (y:ys) = iguales x y ++ eliminaElem x ys
+iguales :: Eq a => a -> a-> [a]
+iguales x y | x == y = []
+            | otherwise = [y]
+
+						
+--cuenta. Dado una entrada con un elemento y una lista, regresa el número de veces que aparece el elemento en la lista.
+--cuenta 3 [1,2,3,3,4] -> 2
+cuenta :: Eq a => a -> [a] -> Int
+cuenta a [] = 0
+cuenta a (x:xs) = if a == x then (1 + (cuenta a xs)) else (0 + (cuenta a xs))
+
+--frecuencia. Dada una lista, regresa una lista de tuplas donde el primer valor de cada tupla corresponde a un elemento de la lista inicial y el segundo valor a la cantidad de veces que aparece tal elemento.
+--frecuencia [1,2,2,3,4,4] -> [(1,1),(2,2),(3,1),(4,2)]
+frecuencia :: Eq a => [a] -> [(a,Int)]
+frecuencia [] = []
+frecuencia (x:xs) = [(x, cuenta x (x:xs))] ++ (frecuencia (filter (/= x) xs))
+
+--inserta. Dado un elemento y una lista, el elemento se inserta en la lista de acuerdo a su valor.
+--inserta. 2 [1,3,4] -> [1,2,3,4]
+inserta :: Ord a => a -> [a] -> [a]
+inserta x [] = [x]
+inserta x (y:ys) = if x <= y then (x:y:ys) else (y:inserta x ys)
+
+--isort. Dada una lista, devuelve una lista ordenada con los mismos elementos
+--isort [3,6,8,4,1,5,2,7,9] -> [1,2,3,4,5,6,7,8,9]
+isort :: Ord a => [a] -> [a]
+isort [] = []
+isort (x:xs) = inserta x (isort xs)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Practica3.hs
@@ -0,0 +1,148 @@
+-- | Estructuras Discretas 2020-1
+-- | Práctica 3: Lógica proposicional
+-- | Profesora: Pilar Selene Linares Arévalo
+-- | Laboratorio: Adrián Felipe Vélez Rivera
+-- | Integrantes: 420004365 juanmanuel32@ciencias.unam.mx
+
+
+module Practica3 where
+
+data Var = A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z deriving (Show,Eq)
+data Formula = Prop Var
+     	     | Verdadero
+	     | Falso
+	     | Neg Formula
+	     | Formula :&: Formula
+	     | Formula :|: Formula
+	     | Formula :=>: Formula
+	     | Formula :<=>: Formula deriving (Show, Eq)
+
+infixl 9 :&:
+infixl 9 :|:
+infixr 8 :=>:
+infixl 7 :<=>:
+
+type Estado = [Var]
+
+negar :: Formula -> Formula
+negar (Verdadero)=Verdadero
+negar (Falso)=Falso
+negar (Prop x)=(Prop x)
+negar (a :&: b)=negar (a) :&: negar (b)
+negar (a :|: b)=negar (a) :|: negar (b)
+negar (Neg a)= case a of
+      	       (c :&: b)->Neg (negar (c)) :|: Neg(negar (b))
+	       (c :|: b)->Neg(negar (c)) :&: Neg(negar (b))
+	       (c :=>: b)->negar (c) :=>: negar (b)
+	       (c :<=>: b)->negar (c) :<=>: negar (b)
+	       _ -> Neg a
+negar (a :=>: b)=negar (a) :=>: negar (b)
+negar (a:<=>: b)=negar (a) :<=>: negar (b)
+
+eliminacion :: Formula -> Formula
+eliminacion (Verdadero)=Verdadero
+eliminacion (Falso)=Falso
+eliminacion (Prop x)=Prop x
+eliminacion (Neg a)= case a of
+	    	     (c :&: b)->Neg (negar(eliminacion(a) :&: eliminacion (b)))
+	       	     (c :|: b)->Neg (negar(eliminacion(a) :|: eliminacion (b)))
+	      	     (c :=>: b)->Neg (negar(Neg (eliminacion(a)) :|: eliminacion (b)))
+	      	     (c :<=>: b)->Neg (negar(eliminacion(a :=>: b) :&: eliminacion(b :=>: a)))
+		     _ -> Neg a
+
+eliminacion (a :&: b)= negar(eliminacion(a) :&: eliminacion (b))
+eliminacion (a :|: b)= negar(eliminacion(a) :|: eliminacion (b))
+eliminacion (a :=>: b) = negar(Neg (eliminacion(a)) :|: eliminacion (b))
+eliminacion (a :<=>: b) = negar(eliminacion(a :=>: b) :&: eliminacion(b :=>: a))
+
+vars :: Formula -> [Var]
+vars (Verdadero) = []
+vars (Falso) = []
+vars (Prop x) = [x]
+vars (Neg a) = case a of
+               (b :|: c) -> vars b ++ vars c
+	       (b :&: c) -> vars b ++ vars c
+	       (b :=>: c) -> vars b ++ vars c
+	       (b :<=>: c) -> vars b ++ vars c
+vars (a :|: b) = vars a ++ vars b
+vars (a :&: b) = vars a ++ vars b
+vars (a :=>: b) = vars a ++ vars b
+vars (a :<=>: b) = vars a ++ vars b
+
+interp :: Estado -> Formula -> Bool
+interp [x] (Verdadero) = True
+interp [x] (Falso) = False
+interp list (Neg a) = case a of
+       	    	      Prop y -> subList (vars (Prop y)) list
+       	    	      (b :&: c) -> if (subList (vars (a)) list && subList (vars(b)) list) then False else True
+		      (b :|: c) -> subList (vars (b)) list == False && subList (vars(c)) list == False
+		      (b :=>: c) -> subList (vars (b)) list  && subList (vars(c)) list == False
+		      (b :<=>: c) -> interp list (b :=>: c) == False || interp list (b :=>: c) == False
+interp list (Prop y) = (vars (Prop y)) == list
+interp list (a :|: b) = subList (vars (a)) list || subList (vars(b)) list
+interp list (a :&: b) = subList (vars (a)) list && subList (vars(b)) list
+interp list (a :=>: b) = subList (vars (a)) list == False || subList (vars(b)) list
+interp list (a :<=>: b) = interp list (a :=>: b) && interp list (b :=>: a)
+
+conjPotencia :: [a] -> [[a]]
+conjPotencia [] = [[]]
+conjPotencia (x:xs) = [x:ys | ys <- conjPotencia xs] ++ conjPotencia xs
+
+estados :: Formula -> [Estado]
+estados (Verdadero) = []
+estados (Falso) = []
+estados (Prop x) = conjPotencia (vars (Prop x))
+estados (Neg a) = case a of
+	     	  (b :&: c) -> conjPotencia (vars (a :|: b))
+		  (b :|: c) -> conjPotencia (vars (a :|: b))
+		  (b :=>: c) -> conjPotencia (vars (a :|: b))
+		  (b :<=>: c) -> conjPotencia (vars (a :|: b))
+estados (a :|: b) = conjPotencia (vars (a :|: b))
+estados (a :&: b) = conjPotencia (vars (a :|: b))
+estados (a :=>: b) = conjPotencia (vars (a :|: b))
+estados (a :<=>: b) = conjPotencia (vars (a :|: b))
+
+tautologia :: Formula -> Bool
+tautologia (Verdadero) = True
+tautologia (Falso) = False
+tautologia (Prop y) = andy [interp (x) (Prop y)|x<-(estados (Prop y)), x /= []]
+tautologia (a :&: b) = andy [interp (x) (a :&: b)|x<-(estados (a :&: b)), x /= []]
+tautologia (a :|: b) = andy [interp (x) (a :|: b)|x<-(estados (a :|: b))]
+tautologia (a :=>: b) = andy [interp (x) (a :=>: b)|x<-(estados (a :=>: b))]
+tautologia (a :<=>: b) = andy [interp (x) (a :<=>: b)|x<-(estados (a :<=>: b))]
+
+contradiccion :: Formula -> Bool
+contradiccion (Verdadero) = False
+contradiccion (Falso) = True
+contradiccion (Prop y) = (andy [interp (x) (Prop y)|x<-(estados (Prop y))]) == False
+contradiccion (Neg a) = case a of
+	      	      	(c :&: b) -> (andy [interp (x) (c :&: b)|x<-(estados (c :&: b))]) 
+	      	      	(c :|: b) -> (andy [interp (x) (c :|: b)|x<-(estados (c :|: b))])
+			(c :=>: b) -> (andy [interp (x) (c :=>: b)|x<-(estados (c :=>: b))])
+			(c :<=>: b) -> (andy [interp (x) (c :<=>: b)|x<-(estados (c :<=>: b))])
+			(Prop y) -> (andy [interp (x) (Prop y)|x<-(estados (Prop y)),x/=[]])
+contradiccion (a :&: b) = (andy [interp (x) (a :&: b)|x<-(estados (a :&: b)),x/=[]]) == False
+contradiccion (a :|: b) = (andy [interp (x) (a :|: b)|x<-(estados (a :|: b)), x/=[]]) == False
+contradiccion (a :=>: b) = (andy [interp (x) (a :=>: b)|x<-(estados (a :=>: b)), x/=[]]) == False
+contradiccion (a :<=>: b) = (andy [interp (x) (a :<=>: b)|x<-(estados (a :<=>: b))]) == False
+
+contingencia :: Formula -> Bool
+contingencia (Verdadero) = False
+contingencia (Falso) = False
+contingencia (Prop y) = contradiccion (Prop y) == False && tautologia (Prop y) == False
+contingencia (a :&: b) = contradiccion (a :&: b) == False && tautologia (a :&: b) == False
+contingencia (a :|: b) = contradiccion (a :|: b) == False && tautologia (a :|: b) == False
+contingencia (a :=>: b) = contradiccion (a :=>: b) == False && tautologia (a :=>: b) == False
+contingencia (a :<=>: b) = contradiccion (a :<=>: b) == False && tautologia (a :<=>: b) == False
+
+subList :: Eq a => [a] -> [a] -> Bool
+subList [] [] = True
+subList _ []    = False
+subList [] _    = True
+subList (x:xs) (y:ys) 
+    | x == y    = subList xs ys   
+    | otherwise = subList (x:xs) ys
+
+andy :: [Bool] -> Bool
+andy [] = True
+andy (x:xs) = if x == True then (andy xs) else False
